98bdd0b0c30ae247a1c9c6a3228ee6c0df421e8c
Chanax2 <148987842+Chanax2@users.noreply.github.com>
2023-10-27 14:47:13 +0200

    Update app.js

--- app.js
+++ app.js
@@ -24,14 +24,20 @@ let obamna = new pokemon ("Obamna","250","200","100","0.7")
 
 while (joe.hp > 0 && obamna.hp > 0) {
     if (isLuck(joe.luck)){
-        console.log("pv enlevé à obamna" +attackPokemon(joe.attack,obamna.defense));
+        console.log("BIDEN BLAST enleve à obamna " +attackPokemon(joe.attack,obamna.defense)+" pv");
         obamna.hp -= attackPokemon(joe.attack,obamna.defense);  
-        console.log("pv de obamna "+obamna.hp);     
+        console.log("pv de obamna à "+obamna.hp);     
+    }
+    else {
+        console.log("Joe rate son biden blast")
     }
     if (isLuck (obamna.luck ) && obamna.hp > 0){
-        console.log("pv enlevé à joe " + attackPokemon(obamna.attack,obamna.defense))
+        console.log("Bombardement de tnt enleve à joe " + attackPokemon(obamna.attack,obamna.defense)+" pv")
         joe.hp -= attackPokemon(obamna.attack,joe.defense)
         console.log("pv de joe " + joe.hp)
+    }
+    else {
+        console.log("Obamna rate son Bombardement de tnt")
     }
     if (joe.hp <= 0) {
         console.log("We're barack")
